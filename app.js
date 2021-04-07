229e3f2650abab5c79a5b2b6c790bffa46b9c863
arnejor <79688268+arnejor@users.noreply.github.com>
2021-04-07 07:46:12 +0200

    push-notifications
    no virker det

--- app.js
+++ app.js
@@ -90,168 +90,12 @@ app.use((req, res, next) => {
 app.use("/", usersRoutes);
 app.use("/notifications", notifications);
 app.use("/", groupRoutes);
+app.use("/", groupRoutes);
 
 
-//////// new version/////////////////////////////////////////////////////////////////
+// push notifications
+let sdb = [];
 
-
-//// for now lets create an array for db, to store all subscriptions
-//let testdb = [];
-//
-//const vapidKeys = {
-//  publicKey:
-//'BO3Z3OvRZVv_N36CUhuGfYBKlELXwVKboqas7Y2szIY4N2_ge0WyxP_Zr6UjXL3I50BofDzBBQVdDVzvJ_90OXM',
-//  privateKey: 'EPzHUZp0eKL-tgC_YJM3lVyPyaP13jpCOi7ox1508B4'
-//};
-//
-//webpush.setVapidDetails(
-//  'mailto:test@test.test',
-//  vapidKeys.publicKey,
-//  vapidKeys.privateKey
-//);
-//
-//
-//app.use(express.static('public'));
-//app.use(bodyparser.urlencoded({extended: true}));
-//app.use(bodyparser.json());
-//
-//
-//app.post('/api/save-subscription/', function (req, res) {
-//  if (!isValidSaveRequest(req, res)) {
-//    return;
-//  }
-//
-//  return saveSubscriptionToDatabase(req.body).then((subscriptionId) => {
-//    console.log(testdb);
-//    console.log('--------------------------------');
-//
-//    res.setHeader('Content-Type', 'application/json');
-//    res.send(JSON.stringify({ data: { success: true } }));
-//  }).catch((err) => {
-//    res.status(500);
-//    res.setHeader('Content-Type', 'application/json');
-//    res.send(JSON.stringify({
-//    error: {
-//      id: 'unable-to-save-subscription',
-//      message: 'The subscription was received but we were unable to save it to our database.'
-//    }
-//    }));
-//  });
-//});
-//
-//const isValidSaveRequest = (req, res) => {
-//  // console.log(req.body);
-//  // console.log('--------------------------------');
-//
-//  // Check the request body has at least an endpoint.
-//  if (!req.body || !req.body.endpoint) {
-//    // Not a valid subscription.
-//    res.status(400);
-//    res.setHeader('Content-Type', 'application/json');
-//    res.send(JSON.stringify({
-//      error: {
-//        id: 'no-endpoint',
-//        message: 'Subscription must have an endpoint.'
-//      }
-//    }));
-//    return false;
-//  }
-//  return true;
-//};
-//
-//const saveSubscriptionToDatabase = (subscription) => {
-//  return new Promise((resolve, reject) => {
-//    // insert into user subscription into db code here
-//    // db.insert(subscription, function(err, newDoc) {
-//    //   if (err) {
-//    //     reject(err);
-//    //     return;
-//    //   }
-//
-//    //   resolve(newDoc._id);
-//    // });
-//
-//    // console.log(db);
-//    testdb.push(subscription)
-//    resolve(true);
-//  });
-//};
-//
-//const getSubscriptionsFromDatabase = () => {
-//  return new Promise(resolve => {
-//    resolve(testdb);
-//  });
-//}
-//
-//
-//
-//app.post('/api/trigger-push-msg/', (req, res) => {
-//  console.log(req.body);
-//  var dataToSend = {};
-//  dataToSend.title = req.body.title;
-//  dataToSend.body = req.body.body;
-//
-//  return getSubscriptionsFromDatabase()
-//  .then((subscriptions) => {
-//    // console.log(subscriptions);
-//    let promiseChain = Promise.resolve();
-//
-//    dataToSend = JSON.stringify(dataToSend);
-//    for (let i = 0; i < subscriptions.length; i++) {
-//      const subscription = subscriptions[i];
-//      promiseChain = promiseChain.then(() => {
-//        console.log('sending notif to: ' + subscription.endpoint);
-//        console.log('');
-//        console.log('');
-//        console.log('');
-//        return triggerPushMsg(subscription, dataToSend);
-//      });
-//    }
-//
-//    return promiseChain;
-//  }).then(() => {
-//    res.setHeader('Content-Type', 'application/json');
-//    console.log(dataToSend);
-//      res.send(JSON.stringify({ data: { success: true } }));
-//  })
-//  .catch(function(err) {
-//    res.status(500);
-//    res.setHeader('Content-Type', 'application/json');
-//    res.send(JSON.stringify({
-//      error: {
-//        id: 'unable-to-send-messages',
-//        message: `We were unable to send messages to all subscriptions : ` +
-//          `'${err.message}'`
-//      }
-//    }));
-//  });
-//
-//
-//});
-//
-//
-//const triggerPushMsg = (subscription, dataToSend) => {
-//  return webpush.sendNotification(subscription, dataToSend)
-//  .catch((err) => {
-//    if (err.statusCode === 410) {
-//      // return deleteSubscriptionFromDatabase(subscription._id);
-//    } else {
-//      console.log('Subscription is no longer valid: ', err);
-//    }
-//  });
-//};
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// order matters. The push notification request must be below the session definition
-// Adding push notifications/////////////////////////////////////////////////////////////////////////////////////////
-//app.use(express.static(path.join(__dirname, "client")));
-//
-//// VAPID keys for push notifications and setting details with web-push module
-//
 const vapidKeys = {
   publicKey:
 "BMEYuTpkeJkCo38EzLhz3e3m8fDpE-oqlIdYZz8HKL4GOQs5EoWLI3B22FO8qDdTvyHGMG9SglTZIqj492yjLxM",
@@ -260,163 +104,46 @@ const vapidKeys = {
 };
 
 webpush.setVapidDetails(
-  'mailto:web-push-book@gauntface.com',
+  'mailto:test@test.test',
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
 
-// returns the subscriptions from the database
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
 
 
 
-// This function triggers the push message. It is called in the logig of sending push notifications above
-const triggerPushMsg = function(subscription, dataToSend) {
-  console.log(subscription);
-  const testSubscription = JSON.stringify(subscription);
-  console.log("trying to send push...")
+app.post('/api/save-subscription/', function (req, res) {
+  if (!isValidSaveRequest(req, res)) {
+    return;
+  }
 
-  //const fakeSubscription = {
-  //  endpoint: "https://fcm.googleapis.com/fcm/send/et1RKlO7A2s:APA91bEiDUg4FCtP5ajbey03c1PGXjr77MSfI0YNMwNbcb6eI0DxMJr1I3twGdHQ_KT1qycXfJGZlJMvqekD1m5-gj2Fh4t5jbwuVvqZeuSD2URLdqc6bEe-dyKuMMj3kYfZwUO2jNJH",
-  //  keys: {
-  //    auth: "ah0a2XmtKMsc2iAWebd80Q",
-  //    p256dh: "BMHJnMeNhG4pbtCyJbZEhTMLrvhO4B4WGvApiNr_j4V2NmO-UIjYDzrlU0V1x_kXx8WIMkhpWKML7WTcYK2TAOY"
-  //  }
-  //};
-  return webpush.sendNotification(subscription, "your push payload text")
-  .catch((err) => {
-    if (err.statusCode === 404 || err.statusCode === 410) {
-      console.log('Subscription has expired or is no longer valid: ', err);
-      return deleteSubscriptionFromDatabase(tsubscription._id);
-    } else {
-      throw err;
-    }
-  });
-};
+  return saveSubscriptionToDatabase(req.body).then((subscriptionId) => {
+    console.log(sdb);
+    console.log('--------------------------------');
 
-// the request for posting a notification. In this test it is via the button.
-app.post('/api/trigger-push-msg/', function (req, res) {
-  const dataToSend = "test, message haha";
-  
-  // BUG HERE. THE getSubscription function does not return a subscription. 
-  return getSubscriptionsFromDatabase()
-  .then(function(subscription) {
-    let promiseChain = Promise.resolve();
-    console.log("trying to push.");
-
-    for (let i = 0; i <= 1; i++) {
-      const subscriptionTest = subscription[i];
-
-      promiseChain = promiseChain.then(() => {
-        console.log("please", );
-        return triggerPushMsg(subscription[i], dataToSend);
-      });
-    
-    }
-    console.log("it worked, actually, but how? Also, what happens now?");
-    return promiseChain;
-    
-  })
-  .then(() => {
     res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({ data: { success: true } }));
-  })
-  .catch(function(err) {
+    res.send(JSON.stringify({ data: { success: true } }));
+  }).catch((err) => {
     res.status(500);
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({
-      error: {
-        id: 'unable-to-send-messages',
-        message: `We were unable to send messages to all subscriptions : ` +
-          `'${err.message}'`
-      }
+    error: {
+      id: 'unable-to-save-subscription',
+      message: 'The subscription was received but we were unable to save it to our database.'
+    }
     }));
-});
-})
-
-
-// saves subscriptions to test DB
-const testDB = [];
-function saveSubscriptionToDatabase(subscription) {
-  return new Promise(function(resolve, reject) {
-
-    testDB.push(subscription);
-    console.log(testDB);
   });
-};
-
-// get subscriptions
-function getSubscriptionsFromDatabase(subscription) {
-  return new Promise(function(resolve, reject) {
-
-    var subscription = testDB;
-
-    console.log("this is from get subscription functions",subscription);
-  
-    resolve(Promise);
-
-  
-
-  });
-  
-}
-
-//const getSubscriptionsFromDatabase = () => {
-//  return new Promise(resolve => {
-//    resolve(testDB);
-//  });
-//}
-
-
-
-// Subscribe route
-app.post("/subscribe", (req, res)=> {
-    const subscription = req.body;
-    res.status(201).json({});
-    const payload = JSON.stringify({title:"Push Test"});
-    webpush.sendNotification(subscription, payload).catch(err=>console.log("ERROR"));
-})
-
-// sending the subscription to back end
-// there are some validation here to make sure the subscription is a real subscription
-app.post('/api/save-subscription/', function (req, res) {
-    console.log("TEST please virk");
-
-    // stringifying the subscription object gottn from client
-    
-
-
-    //if (!isValidSaveRequest(req, res)) {
-    //  return console.log("not valid");
-    //}
-
-    return saveSubscriptionToDatabase(JSON.stringify(req.body))
-    .then(function(subscriptionId) {
-      console.log("it ran through herer");
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({ data: { success: true } }));
-      //console.log("The suscription was sent to server!!!!!", subscriptionId)
-    })
-    .catch(function(err) {
-      res.status(500);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({
-        error: {
-          id: 'unable-to-save-subscription',
-          message: 'The subscription was received but we were unable to save it to our database.'
-        }
-      }));
-    });
 });
 
-
-
-
-
-//here is the validation
 const isValidSaveRequest = (req, res) => {
+  // console.log(req.body);
+  // console.log('--------------------------------');
+
   // Check the request body has at least an endpoint.
-  
   if (!req.body || !req.body.endpoint) {
     // Not a valid subscription.
     res.status(400);
@@ -428,12 +155,92 @@ const isValidSaveRequest = (req, res) => {
       }
     }));
     return false;
-  }}
-//};  
-//
-//
-/////////////////////////////////////////////////////////////////////////////////////////////////////
+  }
+  return true;
+};
 
+const saveSubscriptionToDatabase = (subscription) => {
+  return new Promise((resolve, reject) => {
+    // insert into user subscription into db code here
+    // db.insert(subscription, function(err, newDoc) {
+    //   if (err) {
+    //     reject(err);
+    //     return;
+    //   }
+
+    //   resolve(newDoc._id);
+    // });
+
+    // console.log(db);
+    sdb.push(subscription)
+    resolve(true);
+  });
+};
+
+const getSubscriptionsFromDatabase = () => {
+  return new Promise(resolve => {
+    resolve(sdb);
+  });
+}
+
+
+
+app.post('/api/trigger-push-msg/', (req, res) => {
+  console.log(req.body);
+  var dataToSend = {};
+  dataToSend.title = req.body.title;
+  dataToSend.body = req.body.body;
+
+  return getSubscriptionsFromDatabase()
+  .then((subscriptions) => {
+    // console.log(subscriptions);
+    let promiseChain = Promise.resolve();
+
+    dataToSend = JSON.stringify(dataToSend);
+    for (let i = 0; i < subscriptions.length; i++) {
+      const subscription = subscriptions[i];
+      promiseChain = promiseChain.then(() => {
+        console.log('sending notif to: ' + subscription.endpoint);
+        console.log('');
+        console.log('');
+        console.log('');
+        return triggerPushMsg(subscription, dataToSend);
+      });
+    }
+
+    return promiseChain;
+  }).then(() => {
+    res.redirect("/notifications")
+  
+    //res.setHeader('Content-Type', 'application/json');
+    //  res.send(JSON.stringify({ data: { success: true } 
+  })
+  .catch(function(err) {
+    res.status(500);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+      error: {
+        id: 'unable-to-send-messages',
+        message: `We were unable to send messages to all subscriptions : ` +
+          `'${err.message}'`
+      }
+    }));
+  });
+
+
+});
+
+
+const triggerPushMsg = (subscription, dataToSend) => {
+  return webpush.sendNotification(subscription, dataToSend)
+  .catch((err) => {
+    if (err.statusCode === 410) {
+      // return deleteSubscriptionFromDatabase(subscription._id);
+    } else {
+      console.log('Subscription is no longer valid: ', err);
+    }
+  });
+};
 
 
 
--- public/index.js
+++ public/index.js
@@ -1,157 +1,148 @@
 (function() {
-    var log = document.getElementById('log');
-    const NOTIFICATION_DELAY = 2500;
-  
-    let messageIndex = 0;
-    const fakeMessages = [
-      'Heyo',
-      'Hows it goin?',
-      'What you been up to?',
-      'These aren\'t real messages.',
-    ];
-    
-    const userName = 'arne';
-  
-    const promiseTimeout = function(cb, timeout) {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          cb();
-          resolve();
-        }, timeout);
-      });
-    };
-  
-  
-    const urlBase64ToUint8Array = (base64String) => {
-      const padding = '='.repeat((4 - base64String.length % 4) % 4);
-      const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/')
-      ;
-      const rawData = window.atob(base64);
-      return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
-    }
-  
-    const registerServiceWorker = () => {
-      return new Promise((resolve, reject) => {
-        navigator.serviceWorker.register('service-worker.js')
-        .then((registration) => {
-          console.log('Service worker successfully registered.');
-          console.log('Registration object: ', registration);
-          log.innerHTML += 'Service worker successfully registered.<br>';
-          console.log("service worker detected");
-          registration.pushManager.getSubscription().then((subscription) => {
-            log.innerHTML += 'Subscribing user to push notifications...<br>';
-            if (subscription === null) {
-              // Update UI to ask user to register for Push
-              console.log('Not subscribed to push service!');
-              console.log('Subscribing user to push notifications...');
-  
-              const subscribeOptions = {
-                userVisibleOnly: true,
-                applicationServerKey: urlBase64ToUint8Array(
-                    'BO3Z3OvRZVv_N36CUhuGfYBKlELXwVKboqas7Y2szIY4N2_ge0WyxP_Zr6UjXL3I50BofDzBBQVdDVzvJ_90OXM',
+  var log = document.getElementById('log');
+  const NOTIFICATION_DELAY = 2500;
 
-                )
-              };
-  
-              registration.pushManager.subscribe(subscribeOptions)
-              .then(newSubscription => {
-                console.log('User subscribed to push notifs.');
-                // console.log('Subscription object: ', newSubscription);
-  
-                resolve(newSubscription);
-              })
-            } else {
-              // We have a subscription, update the database
-              console.log('User already subscribed to push notifs.');
-              // console.log('Subscription object: ', subscription);
-  
-              resolve(subscription);
-            }
-          });
-        })
-        .catch((err) => {
-          console.error('Unable to register service worker.');
-          log.innerHTML += 'Unable to register service worker or subscribe user<br>';
-          reject(err);
+  let messageIndex = 0;
+  const fakeMessages = [
+    'Heyo',
+    'Hows it goin?',
+    'What you been up to?',
+    'These aren\'t real messages.',
+  ];
+  const userIcon = '/images/demos/matt-512x512.png';
+  const userName = 'Matt';
+
+  const promiseTimeout = function(cb, timeout) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        cb();
+        resolve();
+      }, timeout);
+    });
+  };
+
+
+  const urlBase64ToUint8Array = (base64String) => {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/')
+    ;
+    const rawData = window.atob(base64);
+    return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
+  }
+
+  const registerServiceWorker = () => {
+    return new Promise((resolve, reject) => {
+      console.log("passed tghis");
+      navigator.serviceWorker.register('service-worker.js')
+      .then((registration) => {
+        console.log('Service worker successfully registered.');
+        console.log('Registration object: ', registration);
+
+        registration.pushManager.getSubscription().then((subscription) => {
+          if (subscription === null) {
+            // Update UI to ask user to register for Push
+            console.log('Not subscribed to push service!');
+            console.log('Subscribing user to push notifications...');
+
+            const subscribeOptions = {
+              userVisibleOnly: true,
+              applicationServerKey: urlBase64ToUint8Array(
+                'BMEYuTpkeJkCo38EzLhz3e3m8fDpE-oqlIdYZz8HKL4GOQs5EoWLI3B22FO8qDdTvyHGMG9SglTZIqj492yjLxM'
+              )
+            };
+
+            registration.pushManager.subscribe(subscribeOptions)
+            .then(newSubscription => {
+              console.log('User subscribed to push notifs.');
+              // console.log('Subscription object: ', newSubscription);
+
+              resolve(newSubscription);
+            })
+          } else {
+            // We have a subscription, update the database
+            console.log('User already subscribed to push notifs.');
+            // console.log('Subscription object: ', subscription);
+
+            resolve(subscription);
+          }
         });
-      });
-    }
-  
-  
-    const sendSubscriptionToBackEnd = (subscription) => {
-      return fetch('/api/save-subscription/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(subscription)
-      }).then((response) => {
-        if (!response.ok) {
-          throw new Error('Bad status code from server.');
-        }
-  
-        console.log('User subscribed to push notifs');
-        log.innerHTML += 'User subscribed to push notifs<br>';
-        return response.json();
-      }).then((responseData) => {
-        if (!(responseData.data && responseData.data.success)) {
-          throw new Error('Bad response from server.');
-        }
-      });
-    }
-  
-    const setUpSWMessageListener = function() {
-      /**** START swMessageListener ****/
-      navigator.serviceWorker.addEventListener('message', function(event) {
-        console.log('Received a message from service worker: ', event.data);
-      });
-      /**** END swMessageListener ****/
-    }
-  
-    const setUpNotificationButtons = function() {
-      setUpSWMessageListener();
-  
-      registerServiceWorker().then((subscription) => {
-        console.log('Subscription object: ', subscription);
-        log.innerHTML += 'subsctiption object: ' + subscription + '<br>';
-        sendSubscriptionToBackEnd(subscription);
-      }).catch((err) => {
-        log.innerHTML += 'some error in subscribing user to to push notifs: ' + err + '<br>';
-        console.err('some error in subscribing user to to push notifs: ', err);
-      });
-    }
-  
-  
-    window.addEventListener('load', function() {
-      if (!('serviceWorker' in navigator)) {
-
-        // Service Worker isn't supported on this browser, disable or hide UI.
-        return;
-      }
-  
-      if (!('PushManager' in window)) {
-        // Push isn't supported on this browser, disable or hide UI.
-        return;
-      }
-      console.log("service worker is found");
-      let promiseChain = new Promise((resolve, reject) => {
-        const permissionPromise = Notification.requestPermission((result) => {
-          resolve(result);
-        }); 
-  
-
-      permissionPromise.then(resolve);
-      }).then((result) => {
-        if (result === 'granted') {
-          console.log("got through here");
-          setUpNotificationButtons();
-        } else {
-          // displayNoPermissionError();
-          console.error('permission not granted');
-        }
+      })
+      .catch((err) => {
+        console.error('Unable to register service worker.');
+        reject(err);
       });
     });
-  })();
-  
+  }
+
+
+  const sendSubscriptionToBackEnd = (subscription) => {
+    return fetch('/api/save-subscription/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(subscription)
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error('Bad status code from server.');
+      }
+
+      console.log('User subscribed to push notifs');
+      return response.json();
+    }).then((responseData) => {
+      if (!(responseData.data && responseData.data.success)) {
+        throw new Error('Bad response from server.');
+      }
+    });
+  }
+
+  const setUpSWMessageListener = function() {
+    /**** START swMessageListener ****/
+    navigator.serviceWorker.addEventListener('message', function(event) {
+      console.log('Received a message from service worker: ', event.data);
+    });
+    /**** END swMessageListener ****/
+  }
+
+  const setUpNotificationButtons = function() {
+    setUpSWMessageListener();
+
+    registerServiceWorker().then((subscription) => {
+      console.log('Subscription object: ', subscription);
+      sendSubscriptionToBackEnd(subscription);
+    }).catch((err) => {
+      console.err('some error in subscribing user to to push notifs: ', err);
+    });
+  }
+
+
+  window.addEventListener('load', function() {
+    if (!('serviceWorker' in navigator)) {
+      // Service Worker isn't supported on this browser, disable or hide UI.
+      return;
+    }
+
+    if (!('PushManager' in window)) {
+      // Push isn't supported on this browser, disable or hide UI.
+      return;
+    }
+
+    let promiseChain = new Promise((resolve, reject) => {
+      const permissionPromise = Notification.requestPermission((result) => {
+        resolve(result);
+      });
+
+      if (permissionPromise)
+        permissionPromise.then(resolve);
+    }).then((result) => {
+      if (result === 'granted') {
+        setUpNotificationButtons();
+      } else {
+        // displayNoPermissionError();
+        console.error('permission not granted');
+      }
+    });
+  });
+})();
